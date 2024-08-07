60cca74aff5801d8d8c8e4adc964a286b9869297
Lauren Solberg <laurensolberg3@gmail.com>
2024-08-06 23:58:38 -0500

    Completed the circles exercise in part 2

--- shapes.js
+++ shapes.js
@@ -13,3 +13,14 @@ svg.selectAll("rect")
         .attr("x", function(d,i){return i*100;})
         .attr("y", function(d,i){return 300 - (d*15)})
         .attr("fill", "orange");
+
+var newX = 300;
+
+        svg.selectAll("circle")
+    .data(dataArray)
+    .enter()
+    .append("circle")
+        .attr("cx", function(d,i){newX += (d*6) + (i*20); return newX;})
+        .attr("cy","100")
+        .attr("r",function(d){ return d*3; })
+        .attr("fill", "black");
