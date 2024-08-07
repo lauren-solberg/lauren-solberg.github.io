c643200228de5623a32189854788ff3ff1dc6aee
Lauren Solberg <laurensolberg3@gmail.com>
2024-08-06 23:05:08 -0500

    Beginning to make a bar chart in shapes.js

--- shapes.js
+++ shapes.js
@@ -1,1 +1,15 @@
-console.log("hello Lauren")
+var dataArray = [5,11,18];
+
+var svg = d3.select("body").append("svg")
+    .attr("height","100%")
+    .attr("width", "100%");
+
+svg.selectAll("rect")
+    .data(dataArray)
+    .enter()
+    .append("rect")
+        .attr("height", function(d){return d*15})
+        .attr("width", "50")
+        .attr("x", function(d,i){return i*100;})
+        .attr("y", function(d,i){return 300 - (d*15)})
+        .attr("fill", "orange");
