6d6a9e03462d7299699d5624a5fc62c7bf0b74de
Lauren Solberg <laurensolberg3@gmail.com>
2024-08-18 21:17:42 -0500

    linear axis attempt 1

--- area.js
+++ area.js
@@ -4,19 +4,44 @@ var dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007',
 var height = 200;
 var width = 500;
 
+var margin = {
+        left:50,
+        right:50,
+        top:40,
+        bottom:0
+};
+
+// y-axis scale:
+var y = d3.scaleLinear()
+                .domain([0, 180]) // domain = input
+                .range([height, 0]); // range = output. Opposite (biggest first) because the chart reads from top left to bottom right
+
+console.log(y(0)); // Checking what the y-value will be when the input (x-value) is 0.
+console.log(y(90));
+console.log(y(180));
+
+// Linear-axis generator:
+// Uses the scale to add elements to the page
+var yAxis = d3.axisRight(y);
+
 // Generator for an area chart:
 var area = d3.area()
                 .x(function(d,i){ return i*20;})
                 .y0(height) // Distance from the top of the screen (where y=0)
-                .y1(function(d){ return height - d;}); // Location of the upper line
+                .y1(function(d){ return y(d);}); // Location of the upper line
 
 // Declare an SVG:
 var svg = d3.select("body")
                 .append("svg")
                 .attr("height","100%")
                 .attr("width", "100%");
+var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
 
 // Actually creating the area chart instance. Calls the enerator with area() and apply the SVG instance with svg.append:
-svg.append("path")
+chartGroup.append("path")
         .attr("d", area(dataArray))
-        
+// Append the axis group here to make the axis actually show up:
+chartGroup.append("g")
+        .attr("class","axis y")
+        .attr("transform","translate("+margin.left+","+margin.top+")")
+        .call(yAxis);    
