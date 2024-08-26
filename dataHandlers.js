a313f0cf2c72fccca4a97eed6d6382d3a2b2a21a
Lauren Solberg <laurensolberg3@gmail.com>
2024-08-26 01:21:11 -0500

    nothing much

--- dataHandlers.js
+++ dataHandlers.js
@@ -12,20 +12,20 @@ d3.tsv("data.tsv")
 
 // DSV: 
 // DSV doesn't go row by row because it sometimes contains entire chapters of text which span many pages and many rows.
-psv = d3.dsvFormat("|");
-d3.text("prices.txt")
-    .get(function(error,data){
-        if(error) {
-            console.error("Error loading data:", error);
-          } else {
-            var rows = psv.parse(data);
-            var newRows = [];
-            for (var p=0; p<rows.length; p++) {
-                newRows.push({month: parseDate(rows[p].month), price: Number(rows[p].price.trim().slice(1))});
-            }
-            console.log(newRows);
-          }
-    })
+// psv = d3.dsvFormat("|");
+// d3.text("prices.txt")
+//     .get(function(error,data){
+//         if(error) {
+//             console.error("Error loading data:", error);
+//           } else {
+//             var rows = psv.parse(data);
+//             var newRows = [];
+//             for (var p=0; p<rows.length; p++) {
+//                 newRows.push({month: parseDate(rows[p].month), price: Number(rows[p].price.trim().slice(1))});
+//             }
+//             console.log(newRows);
+//           }
+//     })
 
 d3.json("treeData.json").get(function(error, data){
   console.log(data);
